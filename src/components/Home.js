602778f9e678f6d3555c39108966891d0231baec
jiapeng tong <jiapengt@student.unimelb.edu.au>
2020-05-24 15:44:15 +0800

    load all data at home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,48 +1,132 @@
-import React, {useState} from "react";
+import React, {Component} from "react";
 import PieChart from "./PieChart";
 import BarChart from "./BarChart";
-import Button from '@material-ui/core/Button';
 
-function Home() {
-  const [num, setNum] = useState(1);
-  return (
-      <div>
-        <Button onClick={() => setNum(1)}>Scenario 1</Button>
-        <Button onClick={() => setNum(2)}>Scenario 2</Button>
-        <Button onClick={() => setNum(3)}>Scenario 3</Button>
-        <Button onClick={() => setNum(4)}>Scenario 4</Button>
-        <Button onClick={() => setNum(5)}>Scenario 5</Button>
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pie_data: {},
+      bar_data: {}
+    };
+  }
 
-        {num === 1 && <div>
-          Scenario 1
-          <div>Pie Chart</div>
-          <PieChart/>
-          <div>Bar Chart</div>
-          <BarChart scenario={1}/>
-        </div>
-        }
+  componentDidMount() {
+    Promise.all([
+      fetch('127.0.0.1/fetch/api/v1.0/tasks/corona'),
+      fetch('127.0.0.1/fetch/api/v1.0/tasks/economy'),
+      fetch('127.0.0.1/fetch/api/v1.0/tasks/employment'),
+    ]).then(async ([corona_data, economy_data, employment_data]) => {
+      const corona = await corona_data.json();
+      const economy = await economy_data.json();
+      const employment = await employment_data.json();
+      // count up all data into one
+      let city_names = [];
+      let city_data = [...corona];
+      let corona_countup = 0;
+      let economy_countup = 0;
+      let employment_countup = 0;
 
-        {num === 2 && <div>
-          Scenario 2
-        </div>
-        }
+      corona.map(e => {
+        corona_countup += e['count'];
+      });
 
-        {num === 3 && <div>
-          Scenario 3
-        </div>
-        }
+      economy.map(e => {
+        economy_countup += e['count'];
+      });
 
-        {num === 4 && <div>
-          Scenario 4
-        </div>
-        }
+      employment.map(e => {
+        employment_countup += e['count'];
+      });
 
-        {num === 5 && <div>
-          Scenario 5
-        </div>
-        }
-      </div>
-  );
+      // combine three datasets as one, for each city
+      city_data.map(e1 => {
+        economy.map(e2 => {
+          if (e1['location name'] === e2['location name']) {
+            e1['count_economy'] = e2['count'];
+          }
+          return e2;
+        });
+        return e1
+      });
+
+      city_data.map(e1 => {
+        employment.map(e2 => {
+          if (e1['location name'] === e2['location name']) {
+            e1['count_employment'] = e2['count'];
+          }
+          return e2;
+        });
+        return e1
+      });
+
+      // get city names in a list
+      city_data.map(e => {
+        city_names.push(e['location name']);
+        return e;
+      });
+
+      return {
+        city_names: city_names,
+        city_data: city_data,
+        corona_countup: corona_countup,
+        economy_countup: economy_countup,
+        employment_countup: employment_countup
+      }
+    }).then((data) => {
+      this.setState(
+          {
+            pie_data: {
+              labels: ['corona', 'economy', 'employment'],
+              datasets: [
+                {
+                  label: 'corona',
+                  data: data.corona_countup,
+                  backgroundColor: '#4ca1ff',
+                },
+                {
+                  label: "economy",
+                  data: data.economy_countup,
+                  backgroundColor: "#ff7474",
+                },
+                {
+                  label: "employment",
+                  data: data.employment_countup,
+                  backgroundColor: "#f8ff58",
+                }
+              ]
+            },
+            bar_data: {
+              labels: data.city_names,
+              datasets: [
+                {
+                  label: 'corona',
+                  data: data.city_data['count'],
+                  backgroundColor: '#4ca1ff',
+                },
+                {
+                  label: "economy",
+                  data: data.city_data['count_economy'],
+                  backgroundColor: "#ff7474",
+                },
+                {
+                  label: "employment",
+                  data: data.city_data['count_employment'],
+                  backgroundColor: "#f8ff58",
+                }]
+            }
+          });
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  render() {
+    return <div>
+      <div>Pie Chart</div>
+      <PieChart data={this.state.pie_data}/>
+      <div>Bar Chart</div>
+      <BarChart data={this.state.bar_data}/>
+    </div>
+  }
 }
-
-export default Home;
--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -5,25 +5,6 @@ import {Pie} from 'react-chartjs-2';
 export default class PieChart extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      labels: ['demo1', 'demo2', 'demo3'],
-      datasets: [{
-        data: [2000, 3000, 4000],
-        // backgroundColor: ['#FF0000', '#0000FF', '#FFFF00']
-      }]
-    }
-  }
-
-  componentDidMount() {
-    // load data based on different areas
-    let url = 'localhost:80/';
-    fetch('http://example.com/movies.json')
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-          // format data into chartjs datasets
-
-        });
   }
 
   render() {
@@ -31,8 +12,8 @@ export default class PieChart extends Component {
         <div>
           <Pie
               data={{
-                labels: this.state.labels,
-                datasets: this.state.datasets
+                labels: this.props.data.labels,
+                datasets: this.props.data.datasets
               }}
           />
         </div>
