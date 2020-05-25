88487a1d82e9d8a4da51a557c7aa9501ac08e958
jiapeng tong <jiapengt@student.unimelb.edu.au>
2020-05-25 15:51:25 +0800

    change data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './components/Home';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
+// import data from './components/data/data1.json';
+import Map from './components/Map';
 
 function App() {
   const [num, setNum] = useState(0);
@@ -11,16 +13,22 @@ function App() {
     setNum(1);
   };
 
+  const setMap = () => {
+    setNum(2);
+  };
+
   return (
       <div className="App">
         <Container maxWidth="md">
           <header style={{display: 'block', width: '100%', height: '50px'}}>
             <Button onClick={setScenarios}>Data Graphs</Button>
-            <Button href={process.env.PUBLIC_URL + '/choropleth.html'}>Choropleth Map</Button>
+            {/*<Button href={process.env.PUBLIC_URL + '/choropleth.html'}>Choropleth Map</Button>*/}
+            <Button onClick={setMap}>Choropleth Map</Button>
           </header>
           </Container>
           {num === 0 && <h3>COMP90024 team54</h3>}
           {num === 1 && <Home/>}
+          {num === 2 && <Map/>}
       </div>
   );
 }
